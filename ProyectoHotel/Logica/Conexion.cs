6354f49a0eb7f5ce3effcc5a3cd8f3bab8c14e5d
Elvis Lara <laraelvisinar@gmail.com>
2024-09-28 00:57:58 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoHotel/Logica/Conexion.cs
@@ -0,0 +1,13 @@
+﻿using DocumentFormat.OpenXml.Drawing.Charts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoHotel.Logica
+{
+    public class Conexion
+    {
+        public static string CN = "Data Source=ELVIS_PC;Initial Catalog = DB_HOTEL; User ID = sa; Password = 12345678;";
+    }
+}
